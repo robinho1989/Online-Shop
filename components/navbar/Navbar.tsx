4779ede4cdc4ab8f4af401e7025ef0e703780d37
Robert <robinho1989@wp.pl>
2022-12-17 13:54:38 +0000

    wrapper added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,9 +1,11 @@
 import { MobileNav } from '../mobileNav/MobileNav';
-
+import styles from './navbar.module.css';
 export const Navbar = () => {
 	return (
-		<nav>
-			<MobileNav />
+		<nav className={styles.navbar}>
+			<div className='wrapper'>
+				<MobileNav />
+			</div>
 		</nav>
 	);
 };
