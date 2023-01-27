2c05cd7df50625b405a341fd8622d9304db08194
Robert <robinho1989@wp.pl>
2023-01-27 19:35:17 +0000

    undefined in productView component

--- views/productView/ProductView.tsx
+++ views/productView/ProductView.tsx
@@ -15,7 +15,7 @@ type ProductViewProps = {
 
 export const ProductView = (product: ProductViewProps) => {
 	const { card, addProduct, removeProduct } = useCardContext();
-	const amountProdcutInCart = card.find(
+	const amountProdcutInCart = card?.find(
 		(item) => item.id === product.id
 	)?.amount;
 
