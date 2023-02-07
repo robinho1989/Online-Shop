8bc2da7c00917a243b383eb1bbb2d1ed5c71bc9d
Robert <robinho1989@wp.pl>
2023-02-07 20:57:13 +0000

    mock typing fixed

--- mocks/product.ts
+++ mocks/product.ts
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker';
 import { Product } from '../contexts/utils/types';
 
 export const generateProduct = (): Product => ({
+	description:faker.commerce.product(),
+	image:faker.image.image(),
 	id: faker.datatype.uuid(),
 	name: faker.commerce.productName(),
 	price: Number(faker.commerce.price()),
@@ -9,6 +11,8 @@ export const generateProduct = (): Product => ({
 });
 
 export const generateProductToFilter=():Product=>({
+	description:faker.commerce.product(),
+	image:faker.image.image(),
 	id:faker.datatype.uuid(),
 	name: faker.commerce.productName(),
 	price: Number(faker.commerce.price()),
